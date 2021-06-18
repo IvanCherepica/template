fcb80fc8927b4d235078c27d003959a9a280349f
Ivan Cherepica <ussr211114@gmail.com>
2021-06-18 23:04:57 +0300

    add store-book-service

---
+++ store-book-service/src/test/java/com/example/storebookservice/StoreBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storebookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
