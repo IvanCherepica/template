06f2aa105ac863abe696e4cdaf6c21b1cc3c05d1
Ivan Cherepica <ussr211114@gmail.com>
2021-06-18 23:00:13 +0300

    add creation-book-service

---
+++ creation-book-service/src/test/java/com/example/creationbookservice/CreationBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creationbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
