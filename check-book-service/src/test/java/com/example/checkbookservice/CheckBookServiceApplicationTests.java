a94374f27082cd68741cdc8ce4396f286da40ac3
Ivan Cherepica <ussr211114@gmail.com>
2021-06-18 23:02:44 +0300

    add check-book-service

---
+++ check-book-service/src/test/java/com/example/checkbookservice/CheckBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
